884916743f44f3aa752a0fa348e4cecdcc0f934e
Prashant Sharma <prashantsharmaxxx1@gmail.com>
2023-02-11 14:50:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,13 @@ import '../styles/App.css';
 const App = () => {
 
   const handleDoubleClick = (event) =>{
-  
+   if (clicks === 1) {
+      console.log('I was double clicked');
+      setClicks(0);
+    } else {
+      setClicks(clicks + 1);
+    }
+  };
   }
   return (
     <div id="main">
