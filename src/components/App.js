f593ed930788cb2b8341ecaf882bc8622e26f8c3
Prashant Sharma <116828172+Prashantsharma3@users.noreply.github.com>
2023-03-04 21:52:10 +0530

    hii

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
-const App = () => {
 
-  const handleDoubleClick = (event) =>{
-   if (clicks === 1) {
+const App = () => {
+  const [clicks, setClicks] = useState(0);
+
+  const handleDoubleClick = () => {
+    if (clicks === 1) {
       console.log('I was double clicked');
       setClicks(0);
     } else {
       setClicks(clicks + 1);
+      console.log('I was not double clicked');
     }
   };
-  }
+
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button id="dblclick-btn" onDoubleClick={handleDoubleClick}>Double click me</button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
+
